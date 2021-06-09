228c2734b9eb26ef4216ae2bf858b48a8cd6b92a
Derpimus <84351529+Derpimus@users.noreply.github.com>
2021-06-09 19:07:04 +0200

    Add files via upload

--- mainxd.js
+++ mainxd.js
@@ -16,32 +16,44 @@ function saveName(){
 
 const addMoreSquares = () => {
 	const square = document.createElement('div')
-	square.classList.add('square')
+	
     score1 = document.getElementById("score1").value
     score2 = document.getElementById("score2").value
     if(score1 > score2){
+        square.classList.add('square')
         square.innerHTML = `
         <div class="square">
-            <span class="name1">${name1}</span>
-            <span class="score1 green">${score1}</span>
-            <span class="myslnik">-</span>
-            <span class="score2 red">${score2}</span>
-            <span class="name2">${name2}</span>
+            <div class="container1">
+                <span class="name1">${name1}</span>
+
+                    <span class="score1 green">${score1}</span>
+                    <span class="myslnik">-</span>
+                    <span class="score2 red">${score2}</span>
+
+                <span class="name2">${name2}</span>
+            </div>
         </div>`
     }
 	if(score2 > score1)
     {
+        square.classList.add('square')
         square.innerHTML = `
-    <div class="square">
-        <span class="name1">${name1}</span>
-        <span class="score1 red">${score1}</span>
-        <span class="myslnik">-</span>
-        <span class="score2 green">${score2}</span>
-        <span class="name2">${name2}</span>
+        <div class="square">
+        <div class="container1">
+            <span class="name1">${name1}</span>
+ 
+                <span class="score1 red">${score1}</span>
+                <span class="myslnik">-</span>
+                <span class="score2 green">${score2}</span>
+
+            <span class="name2">${name2}</span>
+        </div>
     </div>`
     }
     if(score1 == score2)
-    {}
+    {
+
+    }
 
 	tabela.appendChild(square)
 }
