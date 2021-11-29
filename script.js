dd34b0b0af72680ca510cfa1077e92e3736b0ee2
Derpimus <michal.piotr.wysocki@gmail.com>
2021-11-29 20:49:14 +0100

    code optimization + fixed dash in match history

--- script.js
+++ script.js
@@ -9,25 +9,58 @@ let wins = [0, 0];
 let loses = [0, 0];
 let history = [];
 
+const drawWinLose = () => {
+     scoreboard.innerHTML = `
+     <div class="scoreboard-row">
+     <span class="title">Name</span>
+     <span class="title">win-lose</span>
+     <span class="title">+/-</span>
+     </div>`;
+for (let i = 0; i < names.length; i++) {
+     scoreboard.innerHTML += `
+     <div class="scoreboard-row">
+      <span class="name">${names[i]}</span>
+      <span class="score1">${wins[i]}</span>
+      <span class="record">-</span>
+      <span class="score2">${loses[i]}</span>
+      <span class="plus-minus">${scores[i]}</span>
+  </div>`
+}}
+const resetPanel = () => {
+     document.querySelector('#score1').value = 0;
+     document.querySelector('#score2').value = 0;
+     document.querySelector('#player1').value = 'Player1';
+     document.querySelector('#player2').value = 'Player2';
+}
+const drawMatchHistory = () =>{
+     matchHistory.innerHTML = ``;
+     for (let i = 0 ; i < history.length ; i++){
 
+          matchHistory.innerHTML += `
+          <div class="row">
+               <span class="name1">${history[i][0]}</span>
+               <span class="score1">${history[i][2]}</span>
+               <span class="dash">-</span>
+               <span class="score2">${history[i][3]}</span>
+               <span class="name2">${history[i][1]}</span>
+          </div>`
 
-
+     }
+     
+}
 
 btn.addEventListener('click', () => {
      const player1 = document.querySelector('#player1').value;
      const player2 = document.querySelector('#player2').value;
      const score1 = document.querySelector('#score1').value;
      const score2 = document.querySelector('#score2').value;
-     history.push([player1, player2, score1, score2]);
-     if (true) {
-          matchHistory.innerHTML += `
-          <div class="row">
-               <span class="name1">${player1}</span>
-               <span class="score1">${score1}</span>
-               <span class="dash">-</span>
-               <span class="score2">${score2}</span>
-               <span class="name2">${player2}</span>
-          </div>`
+     
+     if ((score1 || score2) > 10) {
+          history.push([player1, player2, score1, score2]);
+
+          drawMatchHistory();
+
+
           if (names[0] == '' && names[1] == '') {
                names[0] = player1;
                names[1] = player2;
@@ -78,29 +111,8 @@ btn.addEventListener('click', () => {
                     }
                }
           }
-          //******************************rysowanie w tabeli */
-          scoreboard.innerHTML = `
-               <div class="scoreboard-row">
-               <span class="title">Name</span>
-               <span class="title">win-lose</span>
-               <span class="title">+/-</span>
-               </div>`;
-          for (let i = 0; i < names.length; i++) {
-               scoreboard.innerHTML += `
-               <div class="scoreboard-row">
-                <span class="name">${names[i]}</span>
-                <span class="score1">${wins[i]}</span>
-                <span class="record">-</span>
-                <span class="score2">${loses[i]}</span>
-                <span class="plus-minus">${scores[i]}</span>
-            </div>`
-          }
-
-          console.log(history);
-          document.querySelector('#score1').value = 0;
-          document.querySelector('#score2').value = 0;
-          document.querySelector('#player1').value = 'Player1';
-          document.querySelector('#player2').value = 'Player2';
+          drawWinLose();
+          resetPanel();
      }
 
 });
